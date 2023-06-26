42a05d0376f06dbb86a121b3281bd09b883958c7
Jai Sawhney <jai.sawhney1@gmail.com>
2023-06-25 23:22:58 -0700

    ChromeDP Setup

--- scraper.go
+++ scraper.go
@@ -1,5 +1,32 @@
 package main
 
-func main() {
+import (
+	"context"
 
+	"github.com/chromedp/chromedp"
+)
+
+type ScrapedData struct {
+	description string
+	link        string
+	vendor      string
+	stockStatus string
+	lastInStock string
+	price       string
+}
+
+const website string = "https://rpilocator.com/?country=US"
+
+func main() {
+	ctx, cancel := chromedp.NewContext(context.Background())
+	defer cancel()
+
+	err := chromedp.Run(ctx,
+		chromedp.Navigate(website),
+		chromedp.WaitNotPresent(`.dataTables_empty`), // Wait for the table to populate
+	)
+
+	if err != nil {
+		panic(err)
+	}
 }
