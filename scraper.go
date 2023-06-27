15685b9cfbac94668f2ac93d88004de373f4785d
Jai Sawhney <jai.sawhney1@gmail.com>
2023-06-27 11:30:40 -0700

    Scrape all  listings

--- scraper.go
+++ scraper.go
@@ -3,33 +3,28 @@ package main
 import (
 	"context"
 	"fmt"
+	"time"
 
 	"github.com/chromedp/cdproto/cdp"
 	"github.com/chromedp/chromedp"
 )
 
-type ScrapedData struct {
+type ProductListing struct {
 	sku         string
 	description string
 	link        string
 	vendor      string
-	stockStatus string
+	inStock     string
 	lastInStock string
 	price       string
 }
 
 const website string = "https://rpilocator.com/?country=US"
 
-func main() {
-	var data []ScrapedData
-
-	ctx, cancel := chromedp.NewContext(context.Background())
-	defer cancel()
-
+// Gets the table rows
+func getRows(ctx context.Context) []*cdp.Node {
 	var rows []*cdp.Node
 	err := chromedp.Run(ctx,
-		chromedp.Navigate(website),
-		chromedp.WaitNotPresent(`.dataTables_empty`), // Wait for the table to populate
 		chromedp.Nodes(
 			"#myTable > tr",
 			&rows,
@@ -41,15 +36,73 @@ func main() {
 		panic(err)
 	}
 
-	for _, row := range rows {
-		var columns []*cdp.Node
-		err = chromedp.Run(ctx,
-			chromedp.Nodes("td", &columns, chromedp.ByQueryAll, chromedp.FromNode(row)),
-		)
+	return rows
+}
 
+// Gets the row columns
+func getColumns(ctx context.Context, row *cdp.Node) []*cdp.Node {
+	var columns []*cdp.Node
+	err := chromedp.Run(ctx,
+		chromedp.Nodes("td", &columns, chromedp.ByQueryAll, chromedp.FromNode(row)),
+	)
+
+	if err != nil {
+		panic(err)
+	}
+
+	return columns
+}
+
+// Get all the listings
+func getListings(ctx context.Context) {
+	var listings []ProductListing
+	var rows = getRows(ctx)
+
+	for _, row := range rows {
+		var listing = ProductListing{}
+
+		// Get the columns for each row
+		var columns = getColumns(ctx, row)
+
+		// Get the text from each column
+		err := chromedp.Run(ctx,
+			chromedp.Text([]cdp.NodeID{columns[0].NodeID}, &listing.sku, chromedp.ByNodeID),
+			chromedp.Text([]cdp.NodeID{columns[1].NodeID}, &listing.description, chromedp.ByNodeID),
+			chromedp.Text([]cdp.NodeID{columns[2].NodeID}, &listing.link, chromedp.ByNodeID),
+			chromedp.Text([]cdp.NodeID{columns[4].NodeID}, &listing.vendor, chromedp.ByNodeID),
+			chromedp.Text([]cdp.NodeID{columns[5].NodeID}, &listing.inStock, chromedp.ByNodeID),
+			chromedp.Text([]cdp.NodeID{columns[6].NodeID}, &listing.lastInStock, chromedp.ByNodeID),
+			chromedp.Text([]cdp.NodeID{columns[7].NodeID}, &listing.price, chromedp.ByNodeID),
+		)
 		if err != nil {
 			panic(err)
 		}
+
+		listings = append(listings, listing)
 	}
-	fmt.Println(data)
+
+	fmt.Println(listings[0])
+}
+
+func main() {
+	ctx, cancel := chromedp.NewContext(context.Background())
+	defer cancel()
+
+	// Set a 15 second timeout
+	ctx, cancel = context.WithTimeout(ctx, 15*time.Second)
+	defer cancel()
+
+	// Navigate and wait for the table to populate
+	err := chromedp.Run(ctx,
+		chromedp.Navigate(website),
+		chromedp.WaitNotPresent(`.dataTables_empty`),
+		chromedp.ActionFunc(func(ctx context.Context) error {
+			getListings(ctx)
+			return nil
+		}),
+	)
+
+	if err != nil {
+		panic(err)
+	}
 }
